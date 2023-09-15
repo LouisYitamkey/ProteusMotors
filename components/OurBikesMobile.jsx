7e153ab1640f08eb7e4340d9e6795638326e5461
Kwaku Louis <bonglamgh@gmail.com>
2023-09-15 11:27:59 +0000

    feat(ourbikes): made ui changes to our bikes component

--- components/OurBikesMobile.jsx
+++ components/OurBikesMobile.jsx
@@ -13,7 +13,7 @@ export const OurBikesMobile = () => {
                   </p>
       <div className="grid grid-cols-2 col-span-2 gap-4 lg:grid-cols-4">
         <figure>
-          <img className="w-full  rounded-md mb-2 py-7 bg-white" src={FirstImage} alt="" width="1310" height="873"/>
+          <img className="w-full  rounded-md mb-2  bg-white" src={FirstImage} alt="" width="1310" height="873"/>
           <span className='font-medium bg-slate-100 bg-white  rounded-md px-3'> Amsterdam E-Bike  </span><br/>
             <a href="#" className="inline-flex mb-2 pl-3 pt-2 items-center justify-center text-base  text-black  hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600">
               Learn more &nbsp; →
